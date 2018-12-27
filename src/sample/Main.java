1a65964a9be484421290a1cfed97dc72f0dbf960
HieJulia <hienminhnguyen711@gmail.com>
2018-12-27 11:42:02 +0200

    add style for desktop size

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,16 +1,30 @@
 package sample;
 
+import javafx.animation.Interpolator;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.VPos;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-
-
+import javafx.util.Duration;
 
 
 /**
@@ -18,33 +32,97 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    // A reference to the model
+    AudioConfigModel acModel = new AudioConfigModel();
+
+    Text textDb;
+    Slider slider;
+    CheckBox mutingCheckBox;
+    ChoiceBox genreChoiceBox;
+    Color color = Color.color(0.66, 0.67, 0.69);
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
 
 
 //        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         // Set title
-        primaryStage.setTitle("Hello World");
+//        primaryStage.setTitle("Hello World");
+//
+//        Group group = new Group();
+//        Scene scene = new Scene(group, 300, 250);
+//
+//        Button btn = new Button();
+//        btn.setLayoutX(100);
+//        btn.setLayoutY(80);
+//
+//        btn.setText("Hello World");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World");
+//            }
+//        });
+//
+//
+//        group.getChildren().add(btn);
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
 
-        Group group = new Group();
-        Scene scene = new Scene(group, 300, 250);
 
-        Button btn = new Button();
-        btn.setLayoutX(100);
-        btn.setLayoutY(80);
+        // Message
+        String message
+                = "Earthrise at Christmas: "
+                + "[Forty] years ago this Christmas, a turbulent world "
+                + "looked to the heavens for a unique view of our home "
+                + "planet. This photo of Earthrise over the lunar horizon "
+                + "was taken by the Apollo 8 crew in December 1968, showing "
+                + "Earth for the first time as it appears from deep space. "
+                + "Astronauts Frank Borman, Jim Lovell and William Anders "
+                + "had become the first humans to leave Earth orbit, "
+                + "entering lunar orbit on Christmas Eve. In a historic live "
+                + "broadcast that night, the crew took turns reading from "
+                + "the Book of Genesis, closing with a holiday wish from "
+                + "Commander Borman: \"We close with good night, good luck, "
+                + "a Merry Christmas, and God bless all of you    all of "
+                + "you on the good Earth.\"";
 
-        btn.setText("Hello World");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        // Reference to the Text
+        Text textRef = new Text(message);
+        textRef.setLayoutY(100);
+        textRef.setTextOrigin(VPos.TOP);
+        textRef.setTextAlignment(TextAlignment.JUSTIFY);
+        textRef.setWrappingWidth(400);
+        textRef.setFill(Color.rgb(187, 195, 107));
+        textRef.setFont(Font.font("SansSerif", FontWeight.BOLD, 24));
 
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World");
-            }
-        });
+        // Provides the animated scrolling behavior for the text
+        TranslateTransition transTransition = new TranslateTransition(new Duration(75000), textRef);
+        transTransition.setToY(-820);
+        transTransition.setInterpolator(Interpolator.LINEAR);
+        transTransition.setCycleCount(Timeline.INDEFINITE);
 
+        // Create an ImageView containing the Image
+        Image image = new Image ("http://projavafx.com/images/earthrise.jpg");
+        ImageView imageView = new ImageView(image);
 
-        group.getChildren().add(btn);
+        // Create a Group containing the text
+        Group textGroup = new Group(textRef);
+        textGroup.setLayoutX(50);
+        textGroup.setLayoutY(180);
+        textGroup.setClip(new Rectangle(430, 85));
+
+        // Combine ImageView and Group to the scene
+        Group root = new Group(imageView, textGroup);
+        Scene scene = new Scene(root, 516, 387);
+
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Hello Earthrise");
         primaryStage.show();
+
+        // Start the text animation
+        transTransition.play();
 
 
 
