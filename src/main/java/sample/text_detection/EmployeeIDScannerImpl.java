3b260577513578557a8371649e17981d0930d29a
hiennguyen@bookygroup.fi <hienminhnguyen711@gmail.com>
2019-01-08 15:46:00 +0200

    update

--- src/main/java/sample/text_detection/EmployeeIDScannerImpl.java
+++ src/main/java/sample/text_detection/EmployeeIDScannerImpl.java
@@ -1,6 +1,8 @@
 package sample.text_detection;
 
 
+import java.io.File;
+
 // Class EmployeeIDScanner Impl
 public class EmployeeIDScannerImpl implements EmployeeIDScanner{
 
@@ -8,6 +10,20 @@ public class EmployeeIDScannerImpl implements EmployeeIDScanner{
     // Get text from image
     @Override
     public String getTextFromImage(String imageFilePath) {
+
+
+        // tao nhin la biet - tao khong thich cai the loai dy
+
+
+
+
+        // ngoi rung dui - hay nhi - no giong con trai nhi - tao thu vi -
+        // cai tinh no la the - no van binh thuong ma - the thoi-
         return null;
     }
 }
+
+
+
+
+
